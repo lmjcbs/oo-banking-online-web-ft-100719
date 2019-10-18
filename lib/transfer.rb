9b371d269099cb6c8bc2ef7097a82005f8398e38
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-18 13:15:54 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
 
   def execute_transaction
-    if valid?
+    if valid? && self.status == "pending"
       sender.balance -= amount
       receiver.balance += amount
       self.status = "complete"
