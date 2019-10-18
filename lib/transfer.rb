553ec704d5a4c7f096deaeab7a9856a9317a0858
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-18 13:24:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,7 @@ class Transfer
         self.status = "complete"
       else
         self.status = "rejected"
-        return"Transaction rejected. Please check your account balance."
+        return "Transaction rejected. Please check your account balance."
       end
     end
   end
